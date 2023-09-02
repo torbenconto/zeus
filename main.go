73286fd5b7b73d510dffe51e12e45d76d6f5032a
pvwnthem <pvwn.them@gmail.com>
2023-09-02 18:24:09 -0400

    test counting sentences with advanced test case

--- main.go
+++ main.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	zeus_lexer "github.com/torbenconto/zeus/lexer"
 )
 
-func main() {
-	input := `Hello, World! This is a "simple text" lexer.`
-
+func CountSentences(input string) int {
 	lexer := zeus_lexer.NewLexer(input)
+	sentenceCount := 0
 
 	for {
 		token := lexer.NextToken()
+		fmt.Println(token)
 		if token.Type == zeus_lexer.Unknown {
 			break
 		}
-		fmt.Printf("Type: %v, Value: %s\n", token.Type, token.Value)
+
+		if strings.ContainsAny(token.Value, ".!?") {
+			sentenceCount++
+		}
 	}
+
+	return sentenceCount
+}
+
+func main() {
+	input := `
+	This is a test sentence. 
+	It contains multiple sentences! Does it really work? 
+	Yes, it does. "But how?" you might ask. 
+	Well, it handles quotes and abbreviations like Mr. and Dr. as well. 
+	And it doesn't get confused by ellipses... or multiple exclamation marks!!!
+  `
+	sentenceCount := CountSentences(input)
+	fmt.Printf("Number of sentences: %d\n", sentenceCount)
 }
