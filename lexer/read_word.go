ce434b473c04179cbcdefdf1bd6bcdfce9bc7db7
pvwnthem <pvwn.them@gmail.com>
2023-09-02 18:03:23 -0400

    read word method

---
+++ lexer/read_word.go
@@ -0,0 +1,16 @@
+package zeus_lexer
+
+import "unicode"
+
+func (l *Lexer) ReadWord() Token {
+
+	start := l.Position
+	for l.Position < len(l.Input) && unicode.IsLetter(rune(l.Input[l.Position])) {
+		l.Position++
+	}
+
+	return Token{
+		Type:  Word,
+		Value: l.Input[start:l.Position],
+	}
+}
