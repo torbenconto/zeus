ecaee2469bc3c8c6164bb91f76617d068ccdf63a
torbenconto <torben@2x1.dev>
2023-09-03 16:37:43 -0400

    add comment to extract emails

--- email.go
+++ email.go
@@ -2,6 +2,7 @@ package zeus
 
 import "regexp"
 
+// Uses regex to extract a given amount of emails (max) from a given string (in). Use a max value of 0 or less to return all emails
 func ExtractEmails(in string, max int) []string {
 	regex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
