fab3af9d20c23ac85bdb5c345f093e17ca06a3f0
torbenconto <torben@2x1.dev>
2023-09-03 14:59:47 -0400

    test

--- lexer/read_word.go
+++ lexer/read_word.go
@@ -8,14 +8,12 @@ func (l *Lexer) readWord() Token {
 	start := l.Position
 	isAbbreviation := false
 
-	// Check if the word starts with a period
-	if l.Position < len(l.Input) && l.Input[l.Position] == '.' {
-		isAbbreviation = true
+	for l.Position < len(l.Input) && (unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-') {
 		l.Position++
 	}
 
-	for l.Position < len(l.Input) && (unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-') {
-		l.Position++
+	if l.Position < len(l.Input) && l.Input[l.Position-1] == '.' {
+		isAbbreviation = true
 	}
 
 	word := l.Input[start:l.Position]
