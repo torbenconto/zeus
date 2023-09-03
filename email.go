30fc8bffe48c064cb25c890758013d684d41b076
torbenconto <torben@2x1.dev>
2023-09-03 16:39:53 -0400

    use simpler regex

--- email.go
+++ email.go
@@ -4,8 +4,7 @@ import "regexp"
 
 // Uses regex to extract a given amount of emails (max) from a given string (in). Use a max value of 0 or less to return all emails
 func ExtractEmails(in string, max int) []string {
-	regex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-
+	regex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	emails := regex.FindAllString(in, -1)
 
 	if max > 0 && max < len(emails) {
