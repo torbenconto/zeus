fa8df3551eb21bfa249c257123c19d55cc434b16
torbenconto <torben@2x1.dev>
2023-09-03 16:31:04 -0400

    fix comment on ExtractUrls

--- url.go
+++ url.go
@@ -2,7 +2,7 @@ package zeus
 
 import "regexp"
 
-// Extracts a given number of urls from the given string (in) using regex. Returns a list of urls with a max length of (max)
+// Extracts a given number of urls from the given string (in) using regex. Returns a list of urls with a max length of (max). Use a max value of less than 0 to return every url
 func ExtractUrls(in string, max int) []string {
 	regex := regexp.MustCompile(`https?://[^\s]+`)
 
