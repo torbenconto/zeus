394c212eede9820635890e53e8488ddb496c8a34
torbenconto <torben@2x1.dev>
2023-09-03 00:42:08 -0400

    more testing

--- lexer/read_quotation.go
+++ lexer/read_quotation.go
@@ -3,16 +3,16 @@ package zeus_lexer
 func (l *Lexer) ReadQuotation() Token {
 	start := l.Position
 	l.Position++
+	/*
+		for l.Position < len(l.Input) && l.Input[l.Position] != '"' {
+			l.Position++
+		}
 
-	for l.Position < len(l.Input) && l.Input[l.Position] != '"' {
+		if l.Position >= len(l.Input) || l.Input[l.Position] != '"' {
+			return Token{Type: Unknown, Value: l.Input[start:l.Position]}
+		}
+
 		l.Position++
-	}
-
-	if l.Position >= len(l.Input) || l.Input[l.Position] != '"' {
-		return Token{Type: Unknown, Value: l.Input[start:l.Position]}
-	}
-
-	l.Position++
-
+	*/
 	return Token{Type: Quotation, Value: l.Input[start:l.Position]}
 }
