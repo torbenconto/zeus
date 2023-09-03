7bc4e70e802e3291413c55c1a62a21a3383deb48
torbenconto <torben@2x1.dev>
2023-09-03 00:40:45 -0400

    revert last commit

--- lexer/read_quotation.go
+++ lexer/read_quotation.go
@@ -1,34 +1,10 @@
 package zeus_lexer
 
-import "unicode"
-
-// ReadQuotation reads and returns a quotation token.
 func (l *Lexer) ReadQuotation() Token {
 	start := l.Position
 	l.Position++
 
 	for l.Position < len(l.Input) && l.Input[l.Position] != '"' {
-		// Check for words inside the quotation.
-		if unicode.IsLetter(rune(l.Input[l.Position])) {
-			// Create a new lexer context for quoted text.
-			quotedLexer := &Lexer{Input: l.Input, Position: l.Position}
-			quotedLexer.SkipWhiteSpace()
-
-			// Read and ignore words inside the quotation.
-			for {
-				quotedToken := quotedLexer.NextToken()
-				if quotedToken.Type == Unknown {
-					break
-				}
-				if quotedToken.Type == Word {
-					// Ignore words inside the quotation.
-				}
-			}
-
-			// Update the main lexer position to the end of the quoted text.
-			l.Position = quotedLexer.Position
-		}
-
 		l.Position++
 	}
 
