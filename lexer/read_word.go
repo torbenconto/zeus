cd3cea3c2e2d66ad5cb82db2a9afff2fab97ec1c
torbenconto <torben@2x1.dev>
2023-09-03 15:18:20 -0400

    tst

--- lexer/read_word.go
+++ lexer/read_word.go
@@ -5,8 +5,20 @@ import "unicode"
 func (l *Lexer) ReadWord() Token {
 
 	start := l.Position
-	for l.Position < len(l.Input) && unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-' {
+	isAbbreviation := false
+
+	for l.Position < len(l.Input) && (unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-') {
+		if l.Input[l.Position] == '.' && l.Position == start+1 {
+			isAbbreviation = true
+		}
 		l.Position++
+	}
+
+	if isAbbreviation {
+		return Token{
+			Type:  Abbreviation,
+			Value: l.Input[start:l.Position],
+		}
 	}
 
 	return Token{
