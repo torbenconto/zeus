c52406e59b9cc12c0d656647b3f1094ca604a38e
torbenconto <torben@2x1.dev>
2023-09-03 14:45:57 -0400

    tests

--- lexer/read_word.go
+++ lexer/read_word.go
@@ -11,27 +11,30 @@ func (l *Lexer) readWord() Token {
 		l.Position++
 	}
 
+	word := l.Input[start:l.Position]
+
 	/*
 		http://people.physics.illinois.edu/Celia/Caps&Acronyms.pdf
 		"In general, common nouns are not capitalized when they're written out as words, but the abbreviations are ALWAYS capitalized—whether they're units, elements, or acronyms. Elements, even those derived from proper names (curium, francium), are always written lower case when they are written out as words."
 		Checking if the word is capitalized and has a period character after it is a not very good, but good enough idea for checking if a word is an abbreviation
 	*/
-	if isAbbreviation(l.Input[start:l.Position]) {
+	if isAbbreviation(word) {
 		return Token{
 			Type:  Abbreviation,
-			Value: l.Input[start:l.Position],
+			Value: word,
 		}
 	}
 
 	return Token{
 		Type:  Word,
-		Value: l.Input[start:l.Position],
+		Value: word,
 	}
 }
 
+// Helper function to check if a word is an abbreviation
 func isAbbreviation(word string) bool {
 	for _, char := range word {
-		if !unicode.IsUpper(char) {
+		if !unicode.IsLetter(char) || !unicode.IsUpper(char) {
 			return false
 		}
 	}
