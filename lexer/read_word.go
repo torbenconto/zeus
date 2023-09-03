9c7b9a32ba342088a154ad8c56573aa2b3b9d6b3
torbenconto <torben@2x1.dev>
2023-09-03 14:48:29 -0400

    test

--- lexer/read_word.go
+++ lexer/read_word.go
@@ -1,29 +1,24 @@
 package zeus_lexer
 
 import (
-	"fmt"
-	"regexp"
+	"unicode"
 )
 
 func (l *Lexer) readWord() Token {
 	start := l.Position
 
-	// Define a regular expression pattern to match abbreviations
+	for l.Position < len(l.Input) && (unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-' || l.Input[l.Position] == '.') {
+		l.Position++
+	}
+
+	word := l.Input[start:l.Position]
+
 	/*
 		http://people.physics.illinois.edu/Celia/Caps&Acronyms.pdf
 		"In general, common nouns are not capitalized when they're written out as words, but the abbreviations are ALWAYS capitalized—whether they're units, elements, or acronyms. Elements, even those derived from proper names (curium, francium), are always written lower case when they are written out as words."
 		Checking if the word is capitalized and has a period character after it is a not very good, but good enough idea for checking if a word is an abbreviation
 	*/
-	abbreviationPattern := regexp.MustCompile(`^[A-Za-z]+(\.[A-Za-z]+)*$`)
-
-	for l.Position < len(l.Input) && (isLetterOrSpecialChar(l.Input[l.Position])) {
-		l.Position++
-	}
-
-	word := l.Input[start:l.Position]
-	fmt.Println(string(word))
-
-	if abbreviationPattern.MatchString(word) {
+	if isAbbreviation(word) {
 		return Token{
 			Type:  Abbreviation,
 			Value: word,
@@ -36,7 +31,12 @@ func (l *Lexer) readWord() Token {
 	}
 }
 
-// Helper function to check if a character is a letter or a special character
-func isLetterOrSpecialChar(char byte) bool {
-	return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || char == '\'' || char == '-' || char == '.'
+// Helper function to check if a word is an abbreviation
+func isAbbreviation(word string) bool {
+	for _, char := range word {
+		if !unicode.IsLetter(char) || !unicode.IsUpper(char) {
+			return false
+		}
+	}
+	return true
 }
