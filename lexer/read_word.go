441df837fa6bc9523ad8e8ad10ec095a402e2a1a
torbenconto <torben@2x1.dev>
2023-09-02 20:22:50 -0400

    fix hyphenation

--- lexer/read_word.go
+++ lexer/read_word.go
@@ -5,7 +5,7 @@ import "unicode"
 func (l *Lexer) ReadWord() Token {
 
 	start := l.Position
-	for l.Position < len(l.Input) && unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' {
+	for l.Position < len(l.Input) && unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-' {
 		l.Position++
 	}
 
