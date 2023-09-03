aa864a0e95df0ae9e63d97eb8f941afc8f2f27c2
torbenconto <torben@2x1.dev>
2023-09-03 15:16:42 -0400

    revert all dumb stuff, retry abbreviations

--- lexer/read_word.go
+++ lexer/read_word.go
@@ -1,38 +1,16 @@
 package zeus_lexer
 
-import (
-	"fmt"
-	"unicode"
-)
+import "unicode"
 
-func (l *Lexer) readWord() Token {
+func (l *Lexer) ReadWord() Token {
+
 	start := l.Position
-	isAbbreviation := false
-
-	for l.Position < len(l.Input) && (unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-' || (l.Input[l.Position] == '.' && !unicode.IsLower(rune(l.Input[l.Position+1])))) {
-		fmt.Println(string(l.Input[l.Position]))
+	for l.Position < len(l.Input) && unicode.IsLetter(rune(l.Input[l.Position])) || l.Input[l.Position] == '\'' || l.Input[l.Position] == '-' {
 		l.Position++
-	}
-
-	if l.Position < len(l.Input) && l.Input[l.Position-1] == '.' {
-		isAbbreviation = true
-	}
-
-	word := l.Input[start:l.Position]
-
-	if !unicode.IsUpper(rune(word[0])) {
-		isAbbreviation = false
-	}
-
-	if isAbbreviation {
-		return Token{
-			Type:  Abbreviation,
-			Value: word,
-		}
 	}
 
 	return Token{
 		Type:  Word,
-		Value: word,
+		Value: l.Input[start:l.Position],
 	}
 }
