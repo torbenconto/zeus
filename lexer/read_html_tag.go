cbf94727412499c656c69580fc2591bcd8017078
torbenconto <torben@2x1.dev>
2023-09-03 22:37:44 -0400

    html

--- lexer/read_html_tag.go
+++ lexer/read_html_tag.go
@@ -1,34 +1,42 @@
 package zeus_lexer
 
-import "unicode"
+import "strings"
 
 func (l *Lexer) readHTMLTag() Token {
 	start := l.Position
 
-	l.Position++
+	// Check if it's an opening tag
+	if strings.HasPrefix(l.Input[l.Position:], "<") {
+		l.Position++ // Move past the '<' character
 
-	if l.Position < len(l.Input) && l.Input[l.Position] == '/' {
-		l.Position++
-		for l.Position < len(l.Input) && l.Input[l.Position] != '>' && !unicode.IsSpace(rune(l.Input[l.Position])) {
+		// Read until '>' is encountered
+		for l.Position < len(l.Input) && l.Input[l.Position] != '>' {
 			l.Position++
 		}
+
 		if l.Position < len(l.Input) && l.Input[l.Position] == '>' {
 			l.Position++
-		} else {
-			return Token{Type: Unknown, Value: l.Input[start:l.Position]}
 		}
+
+		return Token{Type: HTMLTagOpen, Value: l.Input[start:l.Position]}
+	}
+
+	// Check if it's a closing tag
+	if strings.HasPrefix(l.Input[l.Position:], "</") {
+		l.Position += 2 // Move past the '</' characters
+
+		// Read until '>' is encountered
+		for l.Position < len(l.Input) && l.Input[l.Position] != '>' {
+			l.Position++
+		}
+
+		if l.Position < len(l.Input) && l.Input[l.Position] == '>' {
+			l.Position++
+		}
+
 		return Token{Type: HTMLTagClose, Value: l.Input[start:l.Position]}
 	}
 
-	for l.Position < len(l.Input) && l.Input[l.Position] != '>' && !unicode.IsSpace(rune(l.Input[l.Position])) {
-		l.Position++
-	}
-
-	if l.Position < len(l.Input) && l.Input[l.Position] == '>' {
-		l.Position++
-	} else {
-		return Token{Type: Unknown, Value: l.Input[start:l.Position]}
-	}
-
-	return Token{Type: HTMLTagOpen, Value: l.Input[start:l.Position]}
+	// If it's not an HTML tag, return an unknown token
+	return Token{Type: Unknown, Value: l.Input[start:l.Position]}
 }
