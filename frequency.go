31089c808cbb46bfecc0db8b66646847d7157646
torbenconto <torben@2x1.dev>
2023-09-04 22:49:54 -0400

    dbg

--- frequency.go
+++ frequency.go
@@ -53,7 +53,7 @@ func PercentFrequencyOfString(str string, in string) float64 {
 		}
 	}
 
-	fmt.Println(frequency, len(words), frequency/(len(words)-frequency))
+	fmt.Println(frequency, len(words), (len(words) - frequency))
 
 	return float64(frequency / (len(words) - frequency))
 }
